d93423556bef0fb4ceae80bff059945094cb57bd
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-11 12:04:00 +0530

    Added Prime Factor Problem

---
+++ PrimeFactor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgrammingPracticeProblems
+{
+    public static class PrimeFactor
+    {
+        public static void Factorization()
+        {
+            Console.WriteLine("Enter The Number");
+            int num=int.Parse(Console.ReadLine());
+            int i, j;
+            int count = 0;
+            int flag = 0;
+
+            Console.Write("Prime factor of {0}:\n", num);
+            for (i = 2; i < num; i++)
+            {
+                // check for divisibility
+                if (num % i == 0)
+                {
+                    count = 0;
+                    // check for prime number
+                    for (j = 1; j <= i; j++)
+                    {
+                        if (i % j == 0)
+                            count++;
+                    }
+                    if (count == 2)
+                    {
+                        flag = 1;
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+
+            if (flag == 0)
+                Console.Write("There is no Prime factor for {0} ", num);
+        }
+
+    }
+}
