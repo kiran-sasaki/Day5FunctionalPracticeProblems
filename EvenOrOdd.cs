3ae40e7d05dacc96be141114eb6c5b433a775820
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-11 12:06:49 +0530

    Added Even Or Odd Problem

---
+++ EvenOrOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgrammingPracticeProblems
+{
+    public static class EvenOrOdd
+    {
+        public static void Numcheck()
+        {
+            Console.WriteLine("enter the num");
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+            switch (num % 2)
+            {
+                case 0:
+                    Console.WriteLine(num + " is even");
+                    break;
+                case 1:
+                    Console.WriteLine(num + " is odd");
+                    break;
+            }
+        }
+    }
+}
