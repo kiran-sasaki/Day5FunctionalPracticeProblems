9fb090f2dae6f130cc719e5282c48e8d08590770
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-11 12:05:09 +0530

    Added Reminder Quotient Problem

---
+++ RemQuo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgrammingPracticeProblems
+{
+    public static class RemQuo
+    {
+        public static void Divide()
+        {
+            Console.WriteLine("enter the dividend");
+            int dividend = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the dividend:");
+            int divisor = int.Parse(Console.ReadLine());
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend:{0} Divisor:{1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+        }
+    }
+}
