edf61095cc62067dea2d02a93f68ba179f8926b8
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-11 12:09:39 +0530

    Added Main Method

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FunctionalProgrammingPracticeProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //CoinFlip.Percentage();
+            //Harmonic.HarmonicNo();
+            PrimeFactor.Factorization();
+        }
+    }
+}
