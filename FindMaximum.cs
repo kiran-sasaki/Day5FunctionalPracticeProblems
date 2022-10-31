94a9186b225f8b6765f9f34e0bfa4471c133f578
kiran-sasaki <108934828+kiran-sasaki@users.noreply.github.com>
2022-10-31 09:15:55 +0530

    Update FindMaximum.cs

--- FindMaximum.cs
+++ FindMaximum.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace FunctionalProgrammingPracticeProblems
 {
-    internal class FindMaximum
+    public static class FindMaximum
     {
+         public static void GreatestNo()
+        {
+            int num1, num2, num3;
+            Console.WriteLine("Find the largest of three numbers:");
+
+            Console.Write("Input the 1st number :");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the  2nd number :");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the 3rd  number :");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2 || num1 > num3)
+            {
+                Console.Write("The 1st Number is the greatest among three. \n\n");
+            }
+            else if (num2 > num1 || num2 > num3)
+            {
+                Console.Write("The 2nd Number is the greatest among three. \n\n");
+            }
+            if (num3 > num2 || num3 > num1)
+            {
+                Console.Write("The 3rd Number is the greatest among three. \n\n");
+            }
+        }
     }
 }
